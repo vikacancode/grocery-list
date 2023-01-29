8ab966c51a78d1f11c1cf71d41de1c7c3e154a57
unknown <vikvaler7@gmail.com>
2023-01-30 00:38:34 +0300

    first commit

---
+++ src/GroceryList.js
@@ -0,0 +1,70 @@
+import {Component} from 'react';
+import check from './check.png';
+
+export class GroceryList extends Component{
+    state={
+userInput:"",
+groceryList:[]
+}
+
+onChangeEvent(e){
+this.setState ({userInput:e})
+}
+
+addItem (input){
+    if (input === ''){
+        alert ("Please enter an item")
+    } 
+    else{
+let listArray = this.state.groceryList;
+listArray.push (input);
+this.setState ({groceryList:listArray, userInput:''})
+}
+}
+
+deleteItem(){
+    let listArray = this.state.groseryList;
+    listArray=[];
+    this.setState ({groseryList:listArray})
+}
+
+crossedWord(event){
+const li=event.target;
+li.classList.toggle ('crossed');
+}
+
+onFormSubmit(e){
+    e.preventDefault();
+}
+
+render (){
+return(
+<div>
+<form onSubmit={this.onFormSubmit}>
+<div className="container">
+<input type="text" 
+placeholder ="What do you want to buy today?"
+onChange = {(e)=> {this.onChangeEvent (e.target.value)}}
+value={this.state.userInput}/>
+</div>
+<div className="container">
+<button onClick ={()=> this.addItem(this.state.userInput)} className="add">Add</button>
+</div>
+
+<ul>
+    {this.state.groceryList.map((item,index)=>(
+<li onClick ={this.crossedWord}key={index} alt="check-box">
+    <img src={check}width="40px" alt="check-box"/>
+    {item}</li>
+    ))}
+</ul> 
+<div className="container">
+<button onClick ={()=> this.deleteItem()} className="delete">DELETE</button>
+</div>
+</form>
+</div>
+)
+}
+}
+
+
